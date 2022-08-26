69e27ab9baac460491a30b9d63cb1f44efa331a6
Saturday <zhangkairong123@qq.com>
2022-08-26 16:29:09 +0800

    refactor: monorepo

--- packages/smarty-ui-vite/vite.config.ts
+++ packages/smarty-ui-vite/vite.config.ts
@@ -1,49 +1,44 @@
 /// <reference types="vitest" />
-import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
-import vueJsx from '@vitejs/plugin-vue-jsx';
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import vueJsx from "@vitejs/plugin-vue-jsx";
 // import { presetUno, presetAttributify, presetIcons } from 'unocss';
 // import Unocss from 'unocss/vite';
 // import Unocss from './config/unocss'
 import Unocss from "./config/unocss";
 
-
 const rollupOptions = {
-  external: ['vue', 'vue-router'],
+  external: ["vue", "vue-router"],
   output: {
     globals: {
-      vue: 'Vue'
-    }
-  }
-}
+      vue: "Vue",
+    },
+  },
+};
 
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueJsx({}),
-    Unocss()
-  ],
+  plugins: [vue(), vueJsx({}), Unocss()],
   test: {
     globals: true,
     // simulate DOM with happy-dom
     // (requires installing happy-dom as a peer dependency)
-    environment: 'happy-dom',
+    environment: "happy-dom",
     // 支持 tsx 组件
     transformMode: {
-      web: [/.[tj]sx$/]
-    }
+      web: [/.[tj]sx$/],
+    },
   },
   build: {
     rollupOptions,
-    minify: 'terser', // boolean | terser | esbuild
+    minify: "terser", // boolean | terser | esbuild
     sourcemap: true, // 输出 sourcemap
-    brotliSize: true,  // 生成压缩大小报告
+    brotliSize: true, // 生成压缩大小报告
     cssCodeSplit: true,
     lib: {
-      entry: './src/entry.ts',
-      name: 'SmartyUi',
-      fileName: 'smarty-ui',
-      formats: ['es', 'umd', 'iife']
-    }
-  }
-})
+      entry: "./src/entry.ts",
+      name: "SmartyUi",
+      fileName: "smarty-ui",
+      formats: ["es", "umd", "iife"],
+    },
+  },
+});
