b66a7de03d3a61b4e6ae10befae06e499a7552e4
Saturday <zhangkairong123@qq.com>
2022-08-29 15:20:14 +0800

    feat: test

--- scripts/preinstall.js
+++ scripts/preinstall.js
@@ -1,5 +1,5 @@
 if (!/pnpm/.test(process.env.npm_execpath || '')) {
-    console.log('不懂问然叔')
+    console.log('不懂问')
     console.warn(
       `\u001b[33mThis repository requires using pnpm as the package manager ` +
         ` for scripts to work properly.\u001b[39m\n`
