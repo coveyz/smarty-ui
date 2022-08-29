27cb54047ed424c7a3d64f72ac679d82f9659278
xp2000926 <77706310+xp2000926@users.noreply.github.com>
2022-08-30 07:42:10 +0800

    fix: 修改

--- packages/smarty-ui-vite/config/unocss.ts
+++ packages/smarty-ui-vite/config/unocss.ts
@@ -1,5 +1,5 @@
-import { presetAttributify, presetIcons, presetUno } from 'unocss'
-import Unocss from 'unocss/vite'
+import { presetAttributify, presetIcons, presetUno } from "unocss";
+import Unocss from "unocss/vite";
 
 const colors = [
   "white",
@@ -12,8 +12,7 @@ const colors = [
   "indigo",
   "purple",
   "pink",
-]
-
+];
 const icons = [
   "search",
   "edit",
@@ -23,32 +22,32 @@ const icons = [
   "delete",
   "add",
   "share",
-]
-
-
+];
 const safelist = [
-  // ...icons.map(icon => `i-ic-baseline-${icon}`),
-  ...icons.map(icon => `i-ic-baseline-${icon}`),
-  ...colors.map(color => `bg-${color}-100`),
-  ...colors.map(color => `bg-${color}-400`),
-  ...colors.map(color => `bg-${color}-500`),
-  ...colors.map(color => `hover:bg-${color}-100`),
-  ...colors.map(color => `hover:bg-${color}-300`),
-  ...colors.map(color => `hover:bg-${color}-400`),
-  ...colors.map(color => `hover:bg-${color}-500`),
+  ...colors.map((v) => `bg-${v}-100`),
+  ...colors.map((v) => `bg-${v}-400`),
+  ...colors.map((v) => `bg-${v}-500`),
+  ...colors.map((v) => `hover:bg-${v}-100`),
+  ...colors.map((v) => `hover:bg-${v}-300`),
+  ...colors.map((v) => `hover:bg-${v}-400`),
+  ...colors.map((v) => `hover:bg-${v}-500`),
   ...colors.map((v) => `border-${v}-400`),
   ...colors.map((v) => `border-${v}-500`),
-  ...Array.from({ length: 8 }, (_, index) => `px-${index + 1}`),
-  ...Array.from({ length: 8 }, (_, index) => `py-${index + 1}`),
+  ...colors.map((v) => `text-${v}-500`),
+  ...colors.map((v) => `hover:text-${v}-500`),
+  "text-white",
+  ...Array.from({ length: 8 }, (_, i) => `px-${i + 1}`),
+  ...Array.from({ length: 8 }, (_, i) => `py-${i + 1}`),
   ...["xs", "sm", "base", "lg", "xl", "2xl", "3xl"].map((v) => `text-${v}`),
   ...["rounded-full", "rounded-lg"],
-]
-
+  ...icons.map((v) => `i-ic-baseline-${v}`),
+];
 export default () =>
   Unocss({
     safelist,
     presets: [
       presetAttributify(),
       presetIcons(), // 预设图标添加
-      presetUno()]
-  })
+      presetUno(),
+    ],
+  });
